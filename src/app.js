8414c3bda753870a479f5b477952c976a16d031c
Michael J Miller <michael.joseph.miller@gmail.com>
2019-04-26 11:02:54 -0400

    changed default ip and port

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const scraper = require('./modules/scraper')
 require('custom-env').env('dev')
 
 const mongoURL = process.env.MONGO_URL,
-      port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '3000',
-      ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+      port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '8080',
+      ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 mongoose.connect(mongoURL, { useNewUrlParser: true })
 mongoose.set('useFindAndModify', false)
